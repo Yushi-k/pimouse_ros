c8b8cf18b8d9f5c141ed34eda25b377a129d1888
Yushi-k <totoro_7177@yahoo.co.jp>
2021-04-04 03:09:44 +0000

    add script

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -3,8 +3,7 @@
 #required packages
 sudo pip install --upgrade pip
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt update
-sudo apt install python-catkin-pkg
+sudo apt install catkin-pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
