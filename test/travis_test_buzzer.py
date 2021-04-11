ee7fada1f02393aa97cb932fa1731ef6ecced7d5
Yushi-k <totoro_7177@yahoo.co.jp>
2021-04-11 13:02:04 +0000

    Add a test script

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -2,22 +2,22 @@
 import rospy, unittest, rostest
 import rosnode
 import time
-from std_msg import UInt16
+from std_msg.msg import UInt16
 
-#add def
 class BuzzerTest(unittest.TestCase):
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/buzzer',nodes, "node does not exist")
+
     def test_put_value(self):
         pub = rospy.Publisher('/buzzer', UInt16)
         for i in range(10):
             pub.publish(1234)
             time.sleep(0.1)
 
-        with open("/dev/rtbuzzer0",r) as f:
+        with open("/dev/rtbuzzer0","r") as f:
             data = f.readline()
-            self.assertEqual(data,"1234\n","valuedoes not written to rtbuzzer0")
+            self.assertEqual(data,"1234\n","value does not written to rtbuzzer0")
 
 if __name__ == '__main__':
     time.sleep(3)
